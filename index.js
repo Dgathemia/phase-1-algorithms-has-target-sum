be371835fd7da6416e4b4e67ad450514849dd4c3
danielgathemia <danielgathemia01@gmail.com>
2022-08-31 12:44:44 +0300

    complete

--- index.js
+++ index.js
@@ -1,17 +1,36 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const obj = {}
+  for(const num of array){
+    const found = target - num;
+    if(obj[found]){
+      return true;
+    }
+    obj[num] = true;
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  o(n)
 */
 
 /* 
   Add your pseudocode here
+    buld an object to keep track of the numbers that we gone through
+  iterate through the array
+  create a variable to store a value that add up to our target
+  return true if the value is found
+  else return false
 */
 
 /*
   Add written explanation of your solution here
+    create an object to keep track of the numbers in the array
+  that we have gone through
+  For every number in the array, 
+  subtract it from the target and check if the remainder exists in the array
 */
 
 // You can run `node index.js` to view these console logs
